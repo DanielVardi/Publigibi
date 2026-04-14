eb64a6c4d2f4f5c09c386554994f2a991ba41a78
DanielVardi <danielvardi@gmail.com>
2026-04-14 18:45:17 -0400

    Adiciona widget Instagram e botao AO VIVO no menu

---
+++ instagram.js
@@ -0,0 +1,14 @@
+/* CONFIGURACAO DO INSTAGRAM - ALMANAQUE BRASILEIRO
+   Como atualizar o post exibido:
+   1. Abra o post mais recente no Instagram
+   2. Clique nos tres pontinhos → "Incorporar" → copie o codigo
+   3. Extraia o link que aparece em data-instgrm-permalink="..."
+   4. Cole esse link em ULTIMO_POST_URL abaixo
+   5. Salve e publique com o bat
+*/
+
+const INSTAGRAM_USUARIO    = '@almanaquebrasileiro.mtl';
+const INSTAGRAM_PERFIL_URL = 'https://www.instagram.com/almanaquebrasileiro.mtl/';
+
+// Cole aqui a URL do post mais recente (ex: https://www.instagram.com/p/ABC123/)
+const ULTIMO_POST_URL = 'https://www.instagram.com/almanaquebrasileiro.mtl/';
